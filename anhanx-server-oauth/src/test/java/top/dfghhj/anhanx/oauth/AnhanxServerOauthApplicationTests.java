d7da76682e50b85562cb502ec8b8007124f70e09
dfghhj <>
2020-06-03 22:48:10 +0800

    add anhanx-server-oauth

---
+++ anhanx-server-oauth/src/test/java/top/dfghhj/anhanx/oauth/AnhanxServerOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package top.dfghhj.anhanx.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnhanxServerOauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
